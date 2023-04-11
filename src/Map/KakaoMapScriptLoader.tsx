e3a2d2d6ae0abef72eddb186dfd40c9d5d9ce95a
kjun <kjun_all@naver.com>
2023-04-12 00:00:54 +0900

    :sparkles: KakaoMap Api 연결

---
+++ src/Map/KakaoMapScriptLoader.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useEffect, useState } from "react";
+
+interface KaKaoMapScriptLoaderProps {
+  children: ReactNode;
+}
+
+function KakaoMapScriptLoader(props: KaKaoMapScriptLoaderProps) {
+  const [mapScriptLoaded, setMapScriptLoaded] = useState(false);
+  useEffect(() => {
+    const script = document.createElement("script"); // <script></script>
+    script.id = import.meta.env.VITE_KAKAO_MAP_SCRIPT_ID;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${
+      import.meta.env.VITE_KAKAO_MAP_APP_KEY
+    }&autoload=false`;
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        // soccess
+        setMapScriptLoaded(true);
+      });
+    };
+    script.onerror = () => {
+      // fail
+      setMapScriptLoaded(false);
+    };
+
+    document.getElementById("root")?.appendChild(script);
+  }, []);
+
+  return (
+    <>
+      {mapScriptLoaded ? (
+        props.children
+      ) : (
+        <div>
+          <h1>지도를 불러오는 중입니다.</h1>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default KakaoMapScriptLoader;
