03ae7273cb4a78a6bd85754b42e2cb1e5f599a43
kjun <kjun_all@naver.com>
2023-05-20 03:08:45 +0900

    feat: App 컴포넌트, 로직 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import DynamicMap from "./Map/DynamicMap";
 import KakaoMapScriptLoader from "./Map/KakaoMapScriptLoader";
-import SearchLoacation from "./SearchLoacation/SearchLoacation";
+import SearchLoacation from "./SearchLoacation";
+import { PlaceType } from "./types/Types";
+import MapMakerController from "./MapMakerController.tsx";
 
 function App() {
+  const [places, setPlaces] = useState<PlaceType[]>([]);
+  const [selectedPlaceId, setSelectedPlaceId] = useState("");
   return (
     <KakaoMapScriptLoader>
-      <DynamicMap />
-      <SearchLoacation />
+      <DynamicMap>
+        <MapMakerController places={places} selectedPlaceId={selectedPlaceId} />
+        <SearchLoacation
+          onUpdatePlaces={(places) => {
+            setPlaces(places);
+          }}
+          onSelect={(placeId) => {
+            setSelectedPlaceId(placeId);
+          }}
+        />
+      </DynamicMap>
     </KakaoMapScriptLoader>
   );
 }
